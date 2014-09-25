9e8686424fc2eb2eae13861fe0b1ab093cca5a94
HelloSCHM <hello.schm@live.com>
2014-09-25 17:37:47 +0800

    first commit

---
+++ 音乐播放/音乐播放/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Interop;
+using Un4seen.Bass;
+
+namespace 音乐播放
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string filename;
+        int stream;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.IntPtr win = new WindowInteropHelper(this).Handle;
+            //string handle = new WindowInteropHelper(this).Handle.ToInt32();
+            if (!Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_CPSPEAKERS, win))
+            {
+                MessageBox.Show("BASS初始化错误！" + Bass.BASS_ErrorGetCode().ToString());
+            }
+        }
+
+        private void btn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd_Open = new OpenFileDialog();//定义打开文本框实体
+            ofd_Open.Title = "打开文件";//对话框标题
+            ofd_Open.Filter = "文件（.txt）|*.txt|所有文件|*.*";//文件扩展名
+            if ((bool)ofd_Open.ShowDialog().GetValueOrDefault())//打开
+            {
+                filename = ofd_Open.FileName;
+                //成功后的处理
+            }
+        }
+
+        public void Player()
+        {
+            stream = Bass.BASS_StreamCreateFile(filename, 0L, 0L, BASSFlag.BASS_SAMPLE_FLOAT);
+            Bass.BASS_ChannelPlay(stream,true);
+        }
+    }
+}
